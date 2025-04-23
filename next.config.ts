10ba176877d50c997f6c9a17e31d8b7e134b32e2
alexrobaina <alexrobainaph@gmail.com>
2025-04-23 18:07:01 -0300

    fix nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 // next.config.js
-import createNextIntlPlugin from 'next-intl/plugin';
-
-// Pass the path to your i18n configuration file
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['localhost', 'images.unsplash.com'],
-  },
-};
+const nextConfig = {}
 
-export default withNextIntl(nextConfig);
+export default nextConfig
