4de904eea22a51efd44cbea7a245aadafbc67ef4
alexrobaina <alexrobainaph@gmail.com>
2025-04-30 14:48:33 -0300

    fixiong errors

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    siteUrl: 'https://arcarpinteria.vercel.app/',
+    siteUrl: 'https://arcarpinteria.vercel.app',
     generateRobotsTxt: true,
 };
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 // next.config.js
 const withSitemap = require('next-sitemap');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   darkMode: 'class',
   reactStrictMode: true,
   ...withSitemap
+  images: {
+    domains: ['arcarpinteria.vercel.app'],
+  },
 }
 
 export default nextConfig
